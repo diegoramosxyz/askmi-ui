f990a44588a74d951d8fcebf55ee2b0486d88361
Diego Ramos <RamosDiego@pm.me>
2021-06-24 22:13:54 -0400

    started writing contract

--- contracts/Dqanda.sol
+++ contracts/Dqanda.sol
@@ -1,26 +1,112 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 contract Dqanda {
-    string public answer;
-    // Payable address can receive Ether
-    address payable public owner;
+    // Todo: add reentrancy protection
 
-    // Questioners will be identified by their address
-    // The address will map to an array of strings containing 
-    // the hash for each of their question
-    // The Frontend might be used to limit the questions that 
-    // can be asked to prevent the user from using too much gas
-    mapping(address => string[]) public myMap;
+    address public owner;
+    // The price in wei required to ask a question
+    uint256 public price; // This will become a mapping of price tiers
 
-    // Make an event that returns the
-
-    constructor() {
-        answer = "";
+    constructor(uint256 _price) {
         owner = msg.sender;
+        price = _price;
     }
 
-    function respond(string memory _answerHash) public {
-        answer = _answerHash;
+    event QuestionAsked(address questioner, string _questionHash);
+    event QuestionAnswered(
+        address questioner,
+        string _questionHash,
+        string _answerHash
+    );
+
+    struct QandA {
+        string question;
+        string answer;
+        // The balance in wei paid to the owner for a response
+        // This will vary depending on the price tiers
+        uint256 balance;
+    }
+
+    // Questioners can ask multiple questions
+    mapping(address => QandA[]) public QandAs;
+
+    // Helper function to get all of the questions made by
+    // a questioner
+    function getQuestions(address _questioner)
+        public
+        view
+        returns (QandA[] memory)
+    {
+        return QandAs[_questioner];
+    }
+
+    // An array of all questioners. Needed for the UI.
+    address[] public questioners;
+
+    // Helper function to get the complete questioners array.
+    function getQuestioners() public view returns (address[] memory) {
+        return questioners;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Unauthorized: Must be owner.");
+        _;
+    }
+
+    modifier notOwner() {
+        require(msg.sender != owner, "Unauthorized: Must not be owner.");
+        _;
+    }
+
+    // Make the contract payable
+    receive() external payable {}
+
+    // Ensure the questioner is paying the right price
+    modifier coversCost() {
+        require(msg.value == price, "Payment is not equal to the price.");
+        _;
+    }
+
+    // Anyone, but the owner can ask a question
+    function ask(string memory _questionHash)
+        public
+        payable
+        notOwner
+        coversCost
+    {
+        // Save new questioners
+        if (QandAs[msg.sender].length == 0) {
+            questioners.push(msg.sender);
+        }
+        // initialize an empty struct and then update it
+        QandA memory qanda;
+        qanda.question = _questionHash;
+        qanda.balance = msg.value;
+        // qanda.answer initialized to deafult value
+
+        QandAs[msg.sender].push(qanda);
+
+        emit QuestionAsked(msg.sender, _questionHash);
+    }
+
+    // Todo: create a withdraw function for questioners
+
+    // Only the owner can respond and get paid.
+    function respond(
+        address questioner,
+        string memory _questionHash,
+        string memory _answerHash
+    ) public onlyOwner {
+        // Todo: check that question exists
+
+        QandAs[questioner].push(
+            QandA({question: _questionHash, answer: _answerHash, balance: 0})
+        );
+
+        // Todo: Get value from the mapping
+        (bool success, ) = owner.call{value: price}("");
+        require(success, "Failed to send Ether");
+        emit QuestionAnswered(questioner, _questionHash, _answerHash);
     }
 }
